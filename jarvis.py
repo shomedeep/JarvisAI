ed70226e320a0b992e4fd1d80001061a96e44b3a
Shomedeep <shomedeep.mondal1997@gmail.com>
2021-04-17 15:28:22 +0530

    webbrowser added

--- jarvis.py
+++ jarvis.py
@@ -3,6 +3,7 @@ import datetime
 import speech_recognition as sr
 import pyaudio
 import wikipedia
+import webbrowser
 
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty('voices')
@@ -58,3 +59,10 @@ if __name__ == "__main__":
             speak("According to wikipedia")
             print(results)
             speak(results)
+        
+        elif "open youtube" in query:
+            webbrowser.open("youtube.com")
+        elif "open google" in query:
+            webbrowser.open("google.com")
+        elif "open stackoverflow" in query:
+            webbrowser.open("stackoverflow.com")
